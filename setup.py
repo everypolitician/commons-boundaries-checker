42a18dee4dd2347fe96628fc19f3d58763c53484
GeoWill <will@mysociety.org>
2018-05-02 23:28:27 +0100

    Create bare bones Python package for commons-boundaries.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='commons_boundaries',
+    version='0.1',
+    description=(
+        'A collection of tools to assist in creating boundaries in '
+        'democratic commons repoistories'),
+    url='https://github.com/everypolitician/commons-boundaries',
+    author='mySociety',
+    author_email='parliments@mysociety.org',
+    license='MIT',
+    packages=['commons_boundaries'],
+    scripts=['bin/create-csv'],
+    install_requires=[
+        'fiona'
+    ],
+    extras_require={
+        'testing': ['pytest']
+    }
+)
