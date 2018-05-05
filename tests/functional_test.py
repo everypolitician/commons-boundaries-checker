f34ac1e1eb117e5292c5670bc148e569d0bc95a0
GeoWill <will@mysociety.org>
2018-05-05 10:28:51 +0100

    'create-config' script expanded to write boundary-config.json
    This will now uses the BoundaryConfig class along with proj4_from_shp and
    encoding_from_shp functions to fill out the details for boundary-config.json.

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,6 +1,8 @@
-import tempfile
+import json
+import pytest
 import shutil
 import subprocess
+import tempfile
 import unittest
 
 from pathlib import Path
@@ -16,7 +18,6 @@ class AddCountryTest(unittest.TestCase):
         self.src_path = self.tempdir / 'boundaries' / 'source'
         self.src_path.mkdir(parents=True)
         self.config_path = self.tempdir / 'boundaries' / 'boundary-config.json'
-        self.config_path.touch()
 
         # copy a source file there
         camera = Path('./tests/data/italy-source/CAMERA_PLURI_2017')
@@ -29,16 +30,40 @@ class AddCountryTest(unittest.TestCase):
 
     def test_country_directory_is_created(self):
         camera_pluri_path = self.src_path / 'CAMERA_PLURI_2017.shp'
-        subprocess.run(["create-config", str(camera_pluri_path), "country"])
+        subprocess.run(['create-config', str(camera_pluri_path), 'country'],
+                       cwd=str(self.tempdir))
         assert (self.build_path / 'country').is_dir()
 
     def test_can_not_use_same_name_twice(self):
         camera_pluri_path = self.src_path / 'CAMERA_PLURI_2017.shp'
-        subprocess.run(["create-config", str(camera_pluri_path), "country"])
+        subprocess.run(['create-config', str(camera_pluri_path), 'country'],
+                       cwd=str(self.tempdir))
         with pytest.raises(subprocess.CalledProcessError) as e:
-            subprocess.check_output(["create-config",
+            subprocess.check_output(['create-config',
                                     str(camera_pluri_path),
-                                    "country"], stderr=subprocess.STDOUT)
+                                    'country'], stderr=subprocess.STDOUT,
+                                    cwd=str(self.tempdir))
         expected_error = 'country already exists. Please pick a new name\n'
         assert e.value.output.decode('UTF-8') == expected_error
         assert e.value.returncode == 1
+
+    def test_config_for_country_written(self):
+        camera_pluri_path = self.src_path / 'CAMERA_PLURI_2017.shp'
+        subprocess.run(['create-config', str(camera_pluri_path), 'country'],
+                       cwd=str(self.tempdir))
+        with open(str(self.config_path), 'r') as config_json:
+            boundary_config_contents = json.load(config_json)
+        assert boundary_config_contents == [{
+            'boundary': 'country',
+            'source-metadata': {
+                'name': 'CAMERA_PLURI_2017',
+                'crs': '+init=epsg:32632',
+                'encoding': 'UTF-8', 'wikidata-field': ''},
+            'parent': {
+                'parent-name': '',
+                'parent-key': '',
+                'parent-foreign-key': ''},
+            'output': {
+                'method': "MS_FB_PARE + '/{}:{}'.format(row[x], row[y])",
+                'dissolve-all': 'False'}
+        }]
